dc27f41a60724244690b1cdbce24c843a6514c3c
Daniloemejias <daniloemejias@gmail.com>
2023-03-07 02:35:01 +0000

    Generador de Excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#La-excusa").innerHTML = generarExcusa();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+
+let generarExcusa = () => {
+  let pronombre = ["Un", "El"];
+  let sujeto = ["Mapache", "Perro", "mono", "gato"];
+  let verbo = ["se llevo", "se comio", "se robo", "me borro"];
+  let objecto = ["la tarea", "el taller", " el examen"];
+
+  let proIndx = Math.floor(Math.random() * pronombre.length);
+  let subIndx = Math.floor(Math.random() * pronombre.length);
+  let verIndx = Math.floor(Math.random() * pronombre.length);
+  let objIndx = Math.floor(Math.random() * pronombre.length);
+
+  return (
+    pronombre[proIndx] +
+    " " +
+    sujeto[subIndx] +
+    " " +
+    verbo[verIndx] +
+    " " +
+    objecto[objIndx]
+  );
 };
